381f4b8efbea805521bd9ecfc6ca5d7fa461e74a
Carlos Neves <carlos@glencoesoftware.com>
2010-04-05 16:56:09 +0100

    ------------------------------------------------------------------------ r6473 | cneves-x | 2010-04-03 01:50:58 +0100 (Sat, 03 Apr 2010) | 3 lines
    Improving dbhelpers image import handling:
    - Fixing the handling of filenames with spaces
    - On stdout pixels id parsing error, print stderr to console.
    ------------------------------------------------------------------------
    r6472 | cneves-x | 2010-04-03 01:47:46 +0100 (Sat, 03 Apr 2010) | 1 line
    
    Adding LogicalChannel and attributes thereof wrappers in omero.gateway (see #2020).
    ------------------------------------------------------------------------
    r6471 | cneves-x | 2010-04-03 01:45:49 +0100 (Sat, 03 Apr 2010) | 1 line
    
    Removing instrument and logical channel wrappers from gateway, to avoid conflict with new classes in omero.gateway.
    ------------------------------------------------------------------------
    r6468 | cneves-x | 2010-04-02 14:07:27 +0100 (Fri, 02 Apr 2010) | 1 line
    
    Wrapping Instrument and ObjectiveSettings object trees from Image in omero.gateway (see #2020)
    ------------------------------------------------------------------------
    r6464 | cneves-x | 2010-04-01 15:23:39 +0100 (Thu, 01 Apr 2010) | 1 line
    
    Merge Beta4.1 -r6332:6376
    ------------------------------------------------------------------------
    r6371 | cneves-x | 2010-03-24 12:42:25 +0000 (Wed, 24 Mar 2010) | 1 line
    
    omero.gateway linked annotations get permission g+w
    ------------------------------------------------------------------------
    r6370 | cneves-x | 2010-03-24 12:41:19 +0000 (Wed, 24 Mar 2010) | 1 line
    
    omeroweb.extlib removing some more dup and breaking methods from gateway.py
    ------------------------------------------------------------------------
    r6369 | cneves-x | 2010-03-24 12:40:12 +0000 (Wed, 24 Mar 2010) | 1 line
    
    webgateway make sure all login info is removed from django session on logout
    ------------------------------------------------------------------------
    r6359 | cneves-x | 2010-03-23 14:37:33 +0000 (Tue, 23 Mar 2010) | 1 line
    
    omeroweb: adding url handler for static
    ------------------------------------------------------------------------
    r6358 | cneves-x | 2010-03-23 12:50:37 +0000 (Tue, 23 Mar 2010) | 1 line
    
    Removing duplicate listChildren from extlib/gateway, as it is breaking other apps.
    ------------------------------------------------------------------------
    r6351 | cneves-x | 2010-03-22 22:21:10 +0000 (Mon, 22 Mar 2010) | 1 line
    
    omero.gateway: Getting setGroup on sessions working on both 4.1 and 4.2 branches
    ------------------------------------------------------------------------

--- components/tools/OmeroWeb/omeroweb/extlib/gateway.py
+++ components/tools/OmeroWeb/omeroweb/extlib/gateway.py
@@ -2109,12 +2109,12 @@ class OmeroWebObjectWrapper (object):
             else:
                 return 0
     
-    def listAnnotations (self):
-        #container = self._conn.getContainerService()
-        meta = self._conn.getMetadataService()
-        self.annotations = meta.loadAnnotations(self._obj.__class__.__name__, [self._oid], None, None, None).get(self._oid, [])
-        for ann in self.annotations:
-            yield AnnotationWrapper(self._conn, ann)
+    #def listAnnotations (self):
+    #    #container = self._conn.getContainerService()
+    #    meta = self._conn.getMetadataService()
+    #    self.annotations = meta.loadAnnotations(self._obj.__class__.__name__, [self._oid], None, None, None).get(self._oid, [])
+    #    for ann in self.annotations:
+    #        yield AnnotationWrapper(self._conn, ann)
     
     def countAnnotations (self):
         if self.annotation_counter is not None:
@@ -2466,11 +2466,11 @@ class ImageWrapper (OmeroWebObjectWrapper, omero.gateway.ImageWrapper):
         else:
             return ImageImagingEnvironmentWrapper(self._conn, self._obj.imagingEnvironment)
     
-    def getObjectiveSettings(self):
-        if self._obj.objectiveSettings is None:
-            return None
-        else:
-            return ImageObjectiveSettingsWrapper(self._conn, self._obj.objectiveSettings)
+#    def getObjectiveSettings(self):
+#        if self._obj.objectiveSettings is None:
+#            return None
+#        else:
+#            return ImageObjectiveSettingsWrapper(self._conn, self._obj.objectiveSettings)
     
     def getMedium(self):
         if self._obj.objectiveSettings.medium is None:
@@ -2504,94 +2504,94 @@ class ImageWrapper (OmeroWebObjectWrapper, omero.gateway.ImageWrapper):
 
 omero.gateway.ImageWrapper = ImageWrapper
     
-class ChannelWrapper (omero.gateway.ChannelWrapper):
-            
-    def getLogicalChannel(self):
-        meta_serv = self._conn.getMetadataService()
-        if self._obj is None:
-            return None
-        elif self._obj.logicalChannel is None:
-            return None
-        else:
-            lc = meta_serv.loadChannelAcquisitionData([long(self._obj.logicalChannel.id.val)])
-            if lc is not None and len(lc) > 0:
-                return LogicalChannelWrapper(self._conn, lc[0])
-            return None
-            
-omero.gateway.ChannelWrapper = ChannelWrapper
+#class ChannelWrapper (omero.gateway.ChannelWrapper):
+#            
+#    def getLogicalChannel(self):
+#        meta_serv = self._conn.getMetadataService()
+#        if self._obj is None:
+#            return None
+#        elif self._obj.logicalChannel is None:
+#            return None
+#        else:
+#            lc = meta_serv.loadChannelAcquisitionData([long(self._obj.logicalChannel.id.val)])
+#            if lc is not None and len(lc) > 0:
+#                return LogicalChannelWrapper(self._conn, lc[0])
+#            return None
+#            
+#omero.gateway.ChannelWrapper = ChannelWrapper
 
-class LogicalChannelWrapper (OmeroWebObjectWrapper, omero.gateway.BlitzObjectWrapper):
-    
-    def getIllumination(self):
-        if self._obj.illumination is None:
-            return None
-        else:
-            return EnumerationWrapper(self._conn, self._obj.illumination)
-    
-    def getContrastMethod(self):
-        if self._obj.contrastMethod is None:
-            return None
-        else:
-            return EnumerationWrapper(self._conn, self._obj.contrastMethod)
-    
-    def getMode(self):
-        if self._obj.mode is None:
-            return None
-        else:
-            return EnumerationWrapper(self._conn, self._obj.mode)
-    
-    def getEmissionFilter(self):
-        if self._obj.secondaryEmissionFilter is None:
-            return None
-        else:
-            return ImageFilterWrapper(self._conn, self._obj.secondaryEmissionFilter)
-    
-    def getDichroic(self):
-        if self._obj.filterSet is None:
-            return None
-        elif self._obj.filterSet.dichroic is None:
-            return None
-        else:
-            return DichroicWrapper(self._conn, self._obj.filterSet.dichroic)
-    
-    def getDetectorSettings(self):
-        if self._obj.detectorSettings is None:
-            return None
-        elif self._obj.detectorSettings.detector is None:
-            return None
-        else:
-            return ImageDetectorWrapper(self._conn, self._obj.detectorSettings.detector)
-    
-    def getLightSource(self):
-        if self._obj.lightSourceSettings is None:
-            return None
-        elif self._obj.lightSourceSettings.lightSource is None:
-            return None
-        else:
-            return LightSourceWrapper(self._conn, self._obj.lightSourceSettings.lightSource)
-
-class LightSourceWrapper (OmeroWebObjectWrapper, omero.gateway.BlitzObjectWrapper):
-    
-    def getLightSourceType(self):
-        if self._obj.type is None:
-            return None
-        else:
-            return TypeWrapper(self._conn, self._obj.type)
-    
-    def getLaserMedium(self):
-        if self._obj.laserMedium is None:
-            return None
-        else:
-            return EnumerationWrapper(self._conn, self._obj.laserMedium)
-    
-    def getPulse(self):
-        if self._obj.pulse is None:
-            return None
-        else:
-            return EnumerationWrapper(self._conn, self._obj.pulse)
-    
-class DichroicWrapper (OmeroWebObjectWrapper, omero.gateway.BlitzObjectWrapper):
-    pass
+#class LogicalChannelWrapper (OmeroWebObjectWrapper, omero.gateway.BlitzObjectWrapper):
+#    
+#    def getIllumination(self):
+#        if self._obj.illumination is None:
+#            return None
+#        else:
+#            return EnumerationWrapper(self._conn, self._obj.illumination)
+#    
+#    def getContrastMethod(self):
+#        if self._obj.contrastMethod is None:
+#            return None
+#        else:
+#            return EnumerationWrapper(self._conn, self._obj.contrastMethod)
+#    
+#    def getMode(self):
+#        if self._obj.mode is None:
+#            return None
+#        else:
+#            return EnumerationWrapper(self._conn, self._obj.mode)
+#    
+#    def getEmissionFilter(self):
+#        if self._obj.secondaryEmissionFilter is None:
+#            return None
+#        else:
+#            return ImageFilterWrapper(self._conn, self._obj.secondaryEmissionFilter)
+#    
+#    def getDichroic(self):
+#        if self._obj.filterSet is None:
+#            return None
+#        elif self._obj.filterSet.dichroic is None:
+#            return None
+#        else:
+#            return DichroicWrapper(self._conn, self._obj.filterSet.dichroic)
+#    
+#    def getDetectorSettings(self):
+#        if self._obj.detectorSettings is None:
+#            return None
+#        elif self._obj.detectorSettings.detector is None:
+#            return None
+#        else:
+#            return ImageDetectorWrapper(self._conn, self._obj.detectorSettings.detector)
+#    
+#    def getLightSource(self):
+#        if self._obj.lightSourceSettings is None:
+#            return None
+#        elif self._obj.lightSourceSettings.lightSource is None:
+#            return None
+#        else:
+#            return LightSourceWrapper(self._conn, self._obj.lightSourceSettings.lightSource)
+#
+#class LightSourceWrapper (OmeroWebObjectWrapper, omero.gateway.BlitzObjectWrapper):
+#    
+#    def getLightSourceType(self):
+#        if self._obj.type is None:
+#            return None
+#        else:
+#            return TypeWrapper(self._conn, self._obj.type)
+#    
+#    def getLaserMedium(self):
+#        if self._obj.laserMedium is None:
+#            return None
+#        else:
+#            return EnumerationWrapper(self._conn, self._obj.laserMedium)
+#    
+#    def getPulse(self):
+#        if self._obj.pulse is None:
+#            return None
+#        else:
+#            return EnumerationWrapper(self._conn, self._obj.pulse)
+#    
+#class DichroicWrapper (OmeroWebObjectWrapper, omero.gateway.BlitzObjectWrapper):
+#    pass
 
 class DatasetWrapper (OmeroWebObjectWrapper, omero.gateway.DatasetWrapper):
     LINK_NAME = "copyImageLinks"
@@ -2602,22 +2602,22 @@ class DatasetWrapper (OmeroWebObjectWrapper, omero.gateway.DatasetWrapper):
             return '<OmeroWeb%s id=%s>' % (self.__class__.__name__, str(self._oid))
         return super(DatasetWrapper, self).__repr__()
     
-    def listChildren (self):
-        """ return a generator yielding child objects """
-        try:
-            childnodes = [ x.child for x in getattr(self._obj, self.LINK_NAME)()]
-
-            child_ids = [child.id.val for child in childnodes]
-            child_counter = None
-            if len(child_ids) > 0:
-                child_annotation_counter = self._conn.getCollectionCount(self.CHILD, "annotationLinks", child_ids)
-            for child in childnodes:
-                kwargs = dict()
-                if child_annotation_counter:
-                    kwargs['annotation_counter'] = child_annotation_counter.get(child.id.val)
-                yield ImageWrapper(self._conn, child, **kwargs)
-        except:
-            raise NotImplementedError
+#    def listChildren (self):
+#        """ return a generator yielding child objects """
+#        try:
+#            childnodes = [ x.child for x in getattr(self._obj, self.LINK_NAME)()]
+#
+#            child_ids = [child.id.val for child in childnodes]
+#            child_counter = None
+#            if len(child_ids) > 0:
+#                child_annotation_counter = self._conn.getCollectionCount(self.CHILD, "annotationLinks", child_ids)
+#            for child in childnodes:
+#                kwargs = dict()
+#                if child_annotation_counter:
+#                    kwargs['annotation_counter'] = child_annotation_counter.get(child.id.val)
+#                yield ImageWrapper(self._conn, child, **kwargs)
+#        except:
+#            raise NotImplementedError
 
 omero.gateway.DatasetWrapper = DatasetWrapper
 
@@ -2631,27 +2631,27 @@ class ProjectWrapper (OmeroWebObjectWrapper, omero.gateway.ProjectWrapper):
     LINK_NAME = "copyDatasetLinks"
     CHILD = 'Dataset'
     
-    def listChildren (self):
-        """ return a generator yielding child objects """
-        try:
-            childnodes = [ x.child for x in getattr(self._obj, self.LINK_NAME)()]
-
-            child_ids = [child.id.val for child in childnodes]
-            child_counter = None
-            if len(child_ids) > 0:
-                child_counter = self._conn.getCollectionCount(self.CHILD, \
-                    (DatasetWrapper.LINK_NAME[4].lower() + \
-                    DatasetWrapper.LINK_NAME[5:]), child_ids)
-                child_annotation_counter = self._conn.getCollectionCount(self.CHILD, "annotationLinks", child_ids)
-            for child in childnodes:
-                kwargs = dict()
-                if child_counter:
-                    kwargs['child_counter'] = child_counter.get(child.id.val)
-                if child_annotation_counter:
-                    kwargs['annotation_counter'] = child_annotation_counter.get(child.id.val)
-                yield DatasetWrapper(self._conn, child, **kwargs)
-        except:
-            raise NotImplementedError
+#    def listChildren (self):
+#        """ return a generator yielding child objects """
+#        try:
+#            childnodes = [ x.child for x in getattr(self._obj, self.LINK_NAME)()]
+#
+#            child_ids = [child.id.val for child in childnodes]
+#            child_counter = None
+#            if len(child_ids) > 0:
+#                child_counter = self._conn.getCollectionCount(self.CHILD, \
+#                    (DatasetWrapper.LINK_NAME[4].lower() + \
+#                    DatasetWrapper.LINK_NAME[5:]), child_ids)
+#                child_annotation_counter = self._conn.getCollectionCount(self.CHILD, "annotationLinks", child_ids)
+#            for child in childnodes:
+#                kwargs = dict()
+#                if child_counter:
+#                    kwargs['child_counter'] = child_counter.get(child.id.val)
+#                if child_annotation_counter:
+#                    kwargs['annotation_counter'] = child_annotation_counter.get(child.id.val)
+#                yield DatasetWrapper(self._conn, child, **kwargs)
+#        except:
+#            raise NotImplementedError
 
 omero.gateway.ProjectWrapper = ProjectWrapper
 
